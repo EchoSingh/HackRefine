c123ae7365eff4ef1bf7bec8dc23012f571e91d9
Sahaj <sahajguptarocks@gmail.com>
2023-12-24 00:27:02 +0530

    done

--- ML_Model/model_output.py
+++ ML_Model/model_output.py
@@ -31,7 +31,7 @@ def predict_disease(model, image_path):
     result = model.predict(img_array)
     
     # Map the prediction to a human-readable label
-    labels = label=["Apple___Apple_scab suggest Pest-Captan,Chlorothalonil,Copper (Organic","Apple___Black_rot suggest to Pest-Avoid replanting into the same area that contained a previously infected plant.","Apple___Cedar_apple_rust suggest to Pest-Organicdews Neemoil,cinnaprot-proteco,Superkukulus-100ml","Apple___Healthy",
+    labels = ["Apple___Apple_scab suggest Pest-Captan,Chlorothalonil,Copper (Organic","Apple___Black_rot suggest to Pest-Avoid replanting into the same area that contained a previously infected plant.","Apple___Cedar_apple_rust suggest to Pest-Organicdews Neemoil,cinnaprot-proteco,Superkukulus-100ml","Apple___Healthy",
                "Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot suggest to Pest-Copper oxychloride","Corn_(maize)___Common_rust_ suggest to Pest-sulfur or copper-based fungicides",
                "Corn_(maize)___Healthy","Corn_(maize)___Northern_Leaf_Blight suggest to Pest-Mancozeb,chlorothalonil 2","Grape___Black_rot suggest to Pest-Eco Oil,Eco Neem,Eco Traps",
                "Grape___Esca_(Black_Measles)","Grape___Healthy","Grape___Leaf_blight_(Isariopsis_Leaf_Spot)",
@@ -41,7 +41,6 @@ def predict_disease(model, image_path):
                "Tomato___Tomato_Yellow_Leaf_Curl_Virus use pest-agro fertilize to keep healthy","Tomato___Tomato_mosaic_virus use Pest-Agro Plus AM003_1 Pesticide"]
     
     predicted_class = labels[np.argmax(result)]
-    
     return predicted_class
 
 def get_image_path():
