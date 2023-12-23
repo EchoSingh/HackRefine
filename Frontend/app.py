d8ce102b0a8aa7bf36a1ede2908a5fc3409b1730
suvanbanerjee <banerjeesuvan@gmail.com>
2023-12-23 19:28:19 +0530

    results.html

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = '.img//'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
-def hello():
-    return render_template('./Frontend/try.html')
+def index():
+    return render_template('dummy.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    if file.filename == 'ds.jpg':
+        return redirect(request.url)
+
+    if file:
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filename)
+        result = ml_prediction(filename)  # Perform ML prediction when the file is uploaded
+        return redirect(url_for('result', result=result))
+
+@app.route('/result/<result>')
+def result(result):
+    return render_template('result.html', result=result)
+
+def ml_prediction(image_path):
+    return "Example Prediction"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
